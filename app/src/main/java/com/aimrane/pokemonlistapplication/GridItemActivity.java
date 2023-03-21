e1a76d05f33a1b13fe6e28c4d0ad86791e5932b5
aimrane <aimrane.essakhi@gmail.com>
2023-03-21 09:47:25 +0000

    update

--- app/src/main/java/com/aimrane/pokemonlistapplication/GridItemActivity.java
+++ app/src/main/java/com/aimrane/pokemonlistapplication/GridItemActivity.java
@@ -10,12 +10,12 @@ import android.widget.TextView;
 public class GridItemActivity extends AppCompatActivity {
 
     private TextView textView;
-    private int image;
     ImageView selectedImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grid_item);
+
         selectedImage = (ImageView) findViewById(R.id.grid_image); // init a ImageView
         textView = (TextView) findViewById(R.id.item_name); // init a ImageView
         Intent intent = getIntent(); // get Intent which we set from Previous Activity4
